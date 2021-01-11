ac05d5b65b54ed5de7b62550a64b11b09875046d
nathan-potts41 <pottsandpans41@hotmail.com>
2021-01-11 13:39:39 -0600

    added specific api routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method
-    User.findAll()
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -15,6 +17,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
@@ -48,9 +51,10 @@ router.post('/', (req, res) => {
 });
 
 // PUT /api/users/1
-router.put('/', (req, res) => {
-    //expects {username: 'bill', email: 'bill@gmail.com', password: 'password1234'}
-    //if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
     User.update(req.body, {
         where: {
             id: req.params.id
@@ -70,7 +74,7 @@ router.put('/', (req, res) => {
 });
 
 // DELETE /api/users/1
-router.delete('/', (req, res) => {
+router.delete('/:id', (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
